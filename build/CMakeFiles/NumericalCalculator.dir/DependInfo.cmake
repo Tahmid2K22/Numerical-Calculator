50981bebbc0fdfcde982beb8c4e61478ceb4897a
Tamal Ghosh <tamalghosha07@gmail.com>
2025-12-06 09:25:00 +0600

    Bisection and Falsi Position method added

---
+++ build/CMakeFiles/NumericalCalculator.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "E:/2-2/Neumerical/project/Numerical-Calculator/src/algorithm/interpolation/interpolation.cpp" "CMakeFiles/NumericalCalculator.dir/src/algorithm/interpolation/interpolation.cpp.obj" "gcc" "CMakeFiles/NumericalCalculator.dir/src/algorithm/interpolation/interpolation.cpp.obj.d"
+  "E:/2-2/Neumerical/project/Numerical-Calculator/src/algorithm/inverse/naive_inverse.cpp" "CMakeFiles/NumericalCalculator.dir/src/algorithm/inverse/naive_inverse.cpp.obj" "gcc" "CMakeFiles/NumericalCalculator.dir/src/algorithm/inverse/naive_inverse.cpp.obj.d"
+  "E:/2-2/Neumerical/project/Numerical-Calculator/src/algorithm/regression/regression.cpp" "CMakeFiles/NumericalCalculator.dir/src/algorithm/regression/regression.cpp.obj" "gcc" "CMakeFiles/NumericalCalculator.dir/src/algorithm/regression/regression.cpp.obj.d"
+  "E:/2-2/Neumerical/project/Numerical-Calculator/src/main.cpp" "CMakeFiles/NumericalCalculator.dir/src/main.cpp.obj" "gcc" "CMakeFiles/NumericalCalculator.dir/src/main.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/NumericalCalculator.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/NumericalCalculator.dir/src/algorithm/interpolation/interpolation.cpp.obj"
+  "CMakeFiles/NumericalCalculator.dir/src/algorithm/interpolation/interpolation.cpp.obj.d"
+  "CMakeFiles/NumericalCalculator.dir/src/algorithm/inverse/naive_inverse.cpp.obj"
+  "CMakeFiles/NumericalCalculator.dir/src/algorithm/inverse/naive_inverse.cpp.obj.d"
+  "CMakeFiles/NumericalCalculator.dir/src/algorithm/regression/regression.cpp.obj"
+  "CMakeFiles/NumericalCalculator.dir/src/algorithm/regression/regression.cpp.obj.d"
+  "CMakeFiles/NumericalCalculator.dir/src/main.cpp.obj"
+  "CMakeFiles/NumericalCalculator.dir/src/main.cpp.obj.d"
+  "NumericalCalculator.exe"
+  "NumericalCalculator.exe.manifest"
+  "NumericalCalculator.pdb"
+  "libNumericalCalculator.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/NumericalCalculator.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
